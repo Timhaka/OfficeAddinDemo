b3165e1d80e711ada14e4c8e728cc5a75bb5ae27
Tim Håkansson <32096310+Timhaka@users.noreply.github.com>
2018-07-01 02:06:43 +0200

    Update Setup.ps1

--- src/PowerShell/Setup.ps1
+++ src/PowerShell/Setup.ps1
@@ -1,7 +1,7 @@
-Connect-PnPOnline -Url https://folkis2017.sharepoint.com/sites/cgi -UseWebLogin
+Connect-PnPOnline -Url YourSiteUrl -UseWebLogin
 Set-PnPAppSideLoading -On
 
-Apply-PnPProvisioningTemplate -Path "C:\Users\timha\Desktop\Presentation\src\XML\ContentType.xml"
+Apply-PnPProvisioningTemplate -Path "YourPath"
 Add-PnPContentTypeToList -List "Presentation" -ContentType "Demo" -DefaultContentType
 
 Remove-PnPContentTypeFromList -List "Presentation" -ContentType Document
